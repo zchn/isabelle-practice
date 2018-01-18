f4820818ba78179219130fde390ffafeb12820d8
kevinchn <kevinchn@kevinchn99.roam.corp.google.com>
2018-01-17 19:33:20 -0500

    update Exe4p5

--- prog-prove/Exe4p5.thy
+++ prog-prove/Exe4p5.thy
@@ -11,4 +11,25 @@ fun evn :: "nat \<Rightarrow> bool" where
 "evn (Suc 0) = False" |
 "evn (Suc(Suc n)) = evn n"
 
-lemma "
+inductive star :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" where
+refl: "star r x x" |
+step: "r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
+
+inductive iter :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> nat \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" where
+iter_refl: "iter r 0 x x" |
+iter_step: "r x y \<Longrightarrow> iter r n y z \<Longrightarrow> iter r (Suc n) x z"
+
+lemma fixes r n x y
+  assumes s: "iter r n x y"
+  shows "star r x y"
+proof -
+  have "iter r n x y \<Longrightarrow> star r x y"
+  proof (induction rule: iter.induct)
+    case (iter_refl r x)
+    then show ?case by (simp add: star.refl)
+  next
+    case (iter_step r x y n z)
+    then show ?case by (simp add: star.step)
+  qed
+  thus ?thesis using s by simp
+qed
