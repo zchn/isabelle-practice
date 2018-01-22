bb9d2edc786d1c5c8e9d98720b458d487164a76f
kevinchn <kevinchn@kevinchn99.roam.corp.google.com>
2018-01-21 19:32:53 -0500

    Remove Drinker.thy proofs.

--- examples/src/HOL/Isar_Examples/Drinker.thy
+++ examples/src/HOL/Isar_Examples/Drinker.thy
@@ -18,35 +18,9 @@ text \<open>
 lemma de_Morgan:
   assumes "\<not> (\<forall>x. P x)"
   shows "\<exists>x. \<not> P x"
-proof (rule classical)
-  assume "\<nexists>x. \<not> P x"
-  have "\<forall>x. P x"
-  proof
-    fix x show "P x"
-    proof (rule classical)
-      assume "\<not> P x"
-      then have "\<exists>x. \<not> P x" ..
-      with \<open>\<nexists>x. \<not> P x\<close> show ?thesis by contradiction
-    qed
-  qed
-  with \<open>\<not> (\<forall>x. P x)\<close> show ?thesis by contradiction
-qed
-
+  oops
+  
 theorem Drinker's_Principle: "\<exists>x. drunk x \<longrightarrow> (\<forall>x. drunk x)"
-proof cases
-  assume "\<forall>x. drunk x"
-  then have "drunk a \<longrightarrow> (\<forall>x. drunk x)" for a ..
-  then show ?thesis ..
-next
-  assume "\<not> (\<forall>x. drunk x)"
-  then have "\<exists>x. \<not> drunk x" by (rule de_Morgan)
-  then obtain a where "\<not> drunk a" ..
-  have "drunk a \<longrightarrow> (\<forall>x. drunk x)"
-  proof
-    assume "drunk a"
-    with \<open>\<not> drunk a\<close> show "\<forall>x. drunk x" by contradiction
-  qed
-  then show ?thesis ..
-qed
-
+  oops
+  
 end
