c2ea11a8504ae722d11a3b91ed2f332db24250e1
kevinchn <kevinchn@kevinchn99.roam.corp.google.com>
2018-01-22 21:08:34 -0500

    Add some of my solutions to Fib....thy.

--- examples/src/HOL/Isar_Examples/Fibonacci.thy
+++ examples/src/HOL/Isar_Examples/Fibonacci.thy
@@ -24,6 +24,7 @@ text_raw \<open>\<^footnote>\<open>Isar version by Gertrud Bauer. Original tacti
 
 declare One_nat_def [simp]
 
+thm One_nat_def
 
 subsection \<open>Fibonacci numbers\<close>
 
@@ -34,13 +35,28 @@ fun fib :: "nat \<Rightarrow> nat"
   | "fib (Suc (Suc x)) = fib x + fib (Suc x)"
 
 lemma [simp]: "fib (Suc n) > 0"
-  oops
-
+proof (induction "n")
+  case 0
+  then show ?case by simp
+next
+  case Suc
+  then show ?case by simp
+qed
 
 text \<open>Alternative induction rule.\<close>
 
 theorem fib_induct: "P 0 \<Longrightarrow> P 1 \<Longrightarrow> (\<And>n. P (n + 1) \<Longrightarrow> P n \<Longrightarrow> P (n + 2)) \<Longrightarrow> P n"
-  oops
+  for n :: nat
+proof (induct n rule: fib.induct)
+  case 1
+  then show ?case by simp
+next
+  case 2
+  then show ?case by simp
+next
+  case (3 x)
+  then show ?case by simp
+qed
 
 
 subsection \<open>Fib and gcd commute\<close>
@@ -50,17 +66,79 @@ text \<open>A few laws taken from @{cite "Concrete-Math"}.\<close>
 lemma fib_add: "fib (n + k + 1) = fib (k + 1) * fib (n + 1) + fib k * fib n"
   (is "?P n")
   \<comment> \<open>see @{cite \<open>page 280\<close> "Concrete-Math"}\<close>
-  oops
+proof (induct n rule: fib.induct)
+  case 1
+  then show ?case by simp
+next
+  case 2
+  then show ?case by simp
+next
+  case (3 x)
+  then show ?case
+  proof -
+    fix x
+    assume a1: "fib (x + k + 1) = fib (k + 1) * fib (x + 1) + fib k * fib x"
+    assume a2: "fib (Suc x + k + 1) = fib (k + 1) * fib (Suc x + 1) + fib k * fib (Suc x)"
+    have "fib (Suc (Suc x) + k + 1) = fib (x + k + 1) + fib (Suc x + k + 1)" by simp
+    hence "\<dots> = fib (k + 1) * fib (x + 1)
+                + fib k * fib x
+                + fib (k + 1) * fib (Suc x + 1)
+                + fib k * fib (Suc x)" using a1 a2 by simp
+    hence "fib (Suc (Suc x) + k + 1) = fib (k + 1) * (fib (x + 1) + fib (Suc x + 1))
+                                     + fib k * (fib x + fib (Suc x))" by (simp add: add_mult_distrib2)
+    thus "fib (Suc (Suc x) + k + 1) = fib (k + 1) * fib (Suc (Suc x) + 1)
+                                     + fib k * fib (Suc (Suc x))" by simp
+  qed
+qed
 
 lemma gcd_fib_Suc_eq_1: "gcd (fib n) (fib (n + 1)) = 1"
   (is "?P n")
-  oops
+proof (induct n rule: fib_induct)
+  case 1
+  then show ?case by simp
+next
+  case 2
+  then show ?case by simp
+next
+  case (3 n)
+  assume "gcd (fib (n + 1)) (fib (n + 1 + 1)) = 1"
+  assume "gcd (fib n) (fib (n + 1)) = 1"
+  have "fib (Suc (Suc n) + 1) = fib (n + 1) + fib (Suc n + 1)" by simp
+  also have "\<dots> = fib (Suc n + 1) + fib (n + 1)" by simp
+  also have "gcd (fib (Suc n + 1)) \<dots> =
+             gcd (fib (Suc n + 1)) (fib (n + 1))" by (rule gcd_add2)
+  also have "\<dots> = gcd (fib (n + 1)) (fib (Suc n + 1))" by (simp add: gcd.commute)
+  finally have *: "\<dots> = gcd (fib (n + 2)) (fib (n + 2 + 1))" by simp
+  thus "gcd (fib (n + 2)) (fib (n + 2 + 1)) = 1" using 3 by simp
+qed
 
 lemma gcd_mult_add: "(0::nat) < n \<Longrightarrow> gcd (n * k + m) n = gcd m n"
-  oops
-  
+proof -
+  assume "(0::nat) < n"
+  hence "gcd (n * k + m) n = gcd n (m mod n)"
+    by (simp add: gcd_non_0_nat add.commute)
+  also from \<open>0 < n\<close> have "\<dots> = gcd m n" by (simp add: gcd_non_0_nat)
+  finally show ?thesis .
+qed
+
 lemma gcd_fib_add: "gcd (fib m) (fib (n + m)) = gcd (fib m) (fib n)"
-  oops
+proof (cases m)
+  case 0
+  then show ?thesis by simp
+next
+  case (Suc k)
+  hence "gcd (fib m) (fib (n + m)) = gcd (fib (n + k + 1)) (fib (k + 1))"
+    by (simp add: gcd.commute)
+  also have "fib (n + k + 1) = fib (k + 1) * fib (n + 1) + fib k * fib n"
+    by (rule fib_add)
+  also have "gcd \<dots> (fib (k + 1)) = gcd (fib k * fib n) (fib (k + 1))"
+    by (simp add: gcd_mult_add)
+  also have "\<dots> = gcd (fib n) (fib (k + 1))"
+    by (simp only: gcd_fib_Suc_eq_1 gcd_mult_cancel)
+  also have "\<dots> = gcd (fib m) (fib n)"
+    using Suc by (simp add: gcd.commute)
+  finally show ?thesis .
+qed
 
 lemma gcd_fib_diff: "gcd (fib m) (fib (n - m)) = gcd (fib m) (fib n)" if "m \<le> n"
   oops
